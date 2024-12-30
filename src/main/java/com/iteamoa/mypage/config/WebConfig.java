bbf9fc43364fb7537b5cb2b214980701a9d61633
Jeonsangeon <wjstkddjs28@gmail.com>
2024-12-30 17:00:25 +0900

    fix.Reuse CORS

--- src/main/java/com/iteamoa/mypage/config/WebConfig.java
+++ src/main/java/com/iteamoa/mypage/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://iteamoa-frontend.s3-website.ap-northeast-2.amazonaws.com")
-                .allowedMethods("GET",  "PATCH")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
                 .allowCredentials(true);
     }
 
